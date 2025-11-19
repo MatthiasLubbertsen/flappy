82c5622390d785e955f5b81011291e2c1764abd2
Matthias Lubbertsen <Matthiaslubbertsen@gmail.com>
2025-11-19 22:02:22 +0100

    Rename handler function to main and update response

--- api/flappy.py
+++ api/flappy.py
@@ -1,17 +1,13 @@
 import random
 
-def handler(request):
-    # 50/50 kans op up of down
+def main(request):
     if random.choice([True, False]):
         return {
-            "statusCode": 200,
-            "headers": {"Content-Type": "text/plain"},
+            "status": 200,
             "body": "Site is UP"
         }
     else:
         return {
-            "statusCode": 500,
-            "headers": {"Content-Type": "text/plain"},
+            "status": 500,
             "body": "Site is DOWN"
         }
-      
